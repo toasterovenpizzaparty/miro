56ac6d210493467a4f76e2e75b8d3707c734bca2
Patrick van der Werf <patrick@robotkittens.nl>
2020-12-12 18:39:25 +0100

    Removed deprecated files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const port = 3000;
 
 var app = express();
-app.use("/", express.static(__dirname + "/"));
+app.use("/", express.static(__dirname + "/examples"));
 app.listen(port);
 
 console.log("Running server at http://localhost:" + port + "/");
